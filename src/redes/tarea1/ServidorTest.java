d0d1463c3caa8dd760d98d7534f83c4bdba0f27e
ivangolo <gonzalez.ivan235@gmail.com>
2014-04-08 20:17:39 -0300

    Primer archivo .java, nada aun

---
+++ src/redes/tarea1/ServidorTest.java
@@ -0,0 +1,10 @@
+package redes.tarea1;
+
+public class ServidorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
